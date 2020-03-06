c1e6fc665d02a2a84f1d3d4eaacaf6b36ca7dad8
Devil-mYcRy <51002209+Devil-mYcRy@users.noreply.github.com>
2020-03-06 20:30:34 +0300

    feat: finish work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
 exports.min = function min (array) {
-  return 0;
+  return typeof(array)==='undefined'|| array.length===0 ? 0: Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return typeof(array)==='undefined'|| array.length===0 ? 0: Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let summ = 0;
+    if(typeof(array)==='undefined'||array.length===0){
+        return 0;
+    }
+    else{
+        summ = array.reduce((previous, current) => current += previous);
+    }   
+    return summ/=array.length;
 }
